04f8e0634241e8ff94834788bae86c8d23d89bc9
Melvynkoh94 <31084923+Melvynkoh94@users.noreply.github.com>
2020-04-01 17:36:16 +0800

    Replace the Hospital class and separate from the main file

---
+++ Hospital.py
@@ -0,0 +1,40 @@
+from utils import *
+
+#A hospital class
+class Hospital_TEST:
+  def __init__(self, hName, hAddress, hContact, hMapLocation, hRegion, hType, hImage):
+    self.hName = hName
+    self.hAddress = hAddress
+    self.hContact = hContact
+    self.hImage = hImage
+    self.hLocation = hMapLocation
+    self.hNearestSubway = hRegion
+    self.hType = hType
+    self.hPostalCode = extractPostalCode(hAddress)
+
+  def __str__(self):
+    return "{} is located at {}\nPostal Code: {}\nNearest Subway: {}".format(self.hName, self.hAddress, self.hPostalCode, self.hNearestSubway)
+    
+
+#A hospital class
+class Hospital:
+    def __init__(self, hName, hAddress, hContact, hLocation, hNearestSubway, hCategory, hImage, hPostCode):
+        self.hName = hName
+        self.hAddress = hAddress
+        self.hContact = hContact
+        self.hImage = hImage
+        self.hLocation = hLocation
+        self.hNearestSubway = hNearestSubway
+        self.hCategory = hCategory
+        self.hPostCode = hPostCode
+
+class Hospital_Original:
+    def __init__(self, hName, hAddress, hContact, hLocation, hNearestSubway, hCategory, hImage, hPostCode):
+        self.hName = hName
+        self.hAddress = hAddress
+        self.hContact = hContact
+        self.hImage = hImage
+        self.hLocation = hLocation
+        self.hNearestSubway = hNearestSubway
+        self.hCategory = hCategory
+        self.hPostCode = hPostCode
