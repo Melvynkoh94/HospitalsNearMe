3b1c5b0b3e97e6f17ccb189a0eed5e79e4aa9dcd
Melvynkoh94 <31084923+Melvynkoh94@users.noreply.github.com>
2020-04-02 15:14:17 +0800

    Implemented postalCode processing functions

--- utils.py
+++ utils.py
@@ -1,11 +1,5 @@
 import json 
 
-# with open("wheel.json", "r") as f:
-#   wheelInfo = json.loads(f.read())
-#   for value in wheelInfo:
-#     colour_list.append(value['colour'])
-#     text_list.append(value['text'])
-
 hospitals_list = []
 
 # Reads from Json file and then return as a list
@@ -19,10 +13,18 @@ def exportFromJson(filename):
 
 # exportFromJson("hospitals_list.json")
 
-# for i in range(34):
-#   print(hospitals_list[i])
-
 
 def extractPostalCode(str):
   postalCode = str[-6:]
   return postalCode
+
+
+# Given a list of postal codes, returns a sorted list with relative to currentPostalCode
+def sortPostalCodes(postalCode_list, currentPostalCode):
+  currentPostalCode_int = int(currentPostalCode)
+  postalCode_list = [int(i) for i in postalCode_list]
+  #print(postalCode_list)
+
+
+postalCode_list = ['529889', '258500', '768828', '574623', '119074', '609606', '427990', '188770', '544886', '169608', '308433', '159964', '229899', '217562', '228510', '329563', '307677', '289891', '619771', '547530', '539747', '609606', 
+'768024', '169610', '169609', '308433', '308205', '168751', '168582', '544835', '529895', '329562', '569766', '659674']
