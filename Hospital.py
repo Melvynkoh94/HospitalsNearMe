581bad5f71ef340bb3610100340dedcb497b9e78
Melvynkoh94 <31084923+Melvynkoh94@users.noreply.github.com>
2020-04-06 18:18:02 +0800

    Remove irrelavant classes

--- Hospital.py
+++ Hospital.py
@@ -1,7 +1,7 @@
 from utils import *
 
 #A hospital class
-class Hospital_TEST:
+class Hospital:
   def __init__(self, hName, hAddress, hContact, hMapLocation, hRegion, hType, hImage):
     self.hName = hName
     self.hAddress = hAddress
@@ -14,27 +14,3 @@ class Hospital_TEST:
 
   def __str__(self):
     return "{} is located at {}\nPostal Code: {}\nNearest Subway: {}\nMapLocation: {}".format(self.hName, self.hAddress, self.hPostalCode, self.hNearestSubway, self.hLocation)
-
-
-#A hospital class
-class Hospital:
-    def __init__(self, hName, hAddress, hContact, hLocation, hNearestSubway, hCategory, hImage, hPostCode):
-        self.hName = hName
-        self.hAddress = hAddress
-        self.hContact = hContact
-        self.hImage = hImage
-        self.hLocation = hLocation
-        self.hNearestSubway = hNearestSubway
-        self.hCategory = hCategory
-        self.hPostCode = hPostCode
-
-class Hospital_Original:
-    def __init__(self, hName, hAddress, hContact, hLocation, hNearestSubway, hCategory, hImage, hPostCode):
-        self.hName = hName
-        self.hAddress = hAddress
-        self.hContact = hContact
-        self.hImage = hImage
-        self.hLocation = hLocation
-        self.hNearestSubway = hNearestSubway
-        self.hCategory = hCategory
-        self.hPostCode = hPostCode
